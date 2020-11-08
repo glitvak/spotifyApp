e8ebb1b549f0e61bf34ed3e68b8a32347ee8dcfe
Aayush Thapa <aayushthapa051@gmail.com>
2020-11-07 23:06:11 -0500

    firestore tested and works

--- app/src/main/java/com/example/spotifyapp/MainActivity.java
+++ app/src/main/java/com/example/spotifyapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.spotifyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,16 +8,30 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+    private EditText editTextTitle;
+    private EditText editTextDescription;
 
-    private Button add;
-    private Button reset;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button save;
     private Button red;
     private Button black;
     private Button blue;
@@ -25,28 +40,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView countText = findViewById(R.id.count);
+        editTextDescription = findViewById(R.id.description);
+        editTextTitle = findViewById(R.id.edit_text_title);
+
         final RelativeLayout layout = findViewById(R.id.layout);
-        add = findViewById(R.id.add);
-        reset = findViewById(R.id.reset);
+
+        save = findViewById(R.id.save);
         red = findViewById(R.id.redButton);
         black = findViewById(R.id.blackButton);
         blue = findViewById(R.id.blueButton);
 
-        add.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                int countNum = Integer.parseInt(countText.getText().toString());
-                countText.setText(countNum + 1 + "");
-            }
-        });
-        reset.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                countText.setText(0 + "");
-                layout.setBackgroundColor(Color.GRAY);
-            }
-        });
 
         red.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -56,8 +59,7 @@ public class MainActivity extends AppCompatActivity {
         });
         black.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                layout.setBackgroundColor(Color.BLACK);
+            public void onClick(View v) { layout.setBackgroundColor(Color.BLACK);
             }
         });
         blue.setOnClickListener(new View.OnClickListener(){
@@ -67,5 +69,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public void saveNote(View v) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_TITLE, title);
+        note.put(KEY_DESCRIPTION, description);
+        db.collection("Notebook").document("My First Note").set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
     }
 }
